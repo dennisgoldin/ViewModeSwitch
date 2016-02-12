1e831ec5b3749c0480bd1ab3120746686e52a655
Dennis Goldin <dennis.goldin@gmail.com>
2016-02-12 18:43:50 -0500

    Add nav bar CSS.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,36 @@
-var myTest = document.querySelector("#test");
-var myGridStyle = document.querySelector("#grid");
-var myListStyle = document.querySelector("#list");
 
-myTest.addEventListener('click', function () {
-  console.log(myGridStyle.disabled);
-  if (!myGridStyle.disabled) {
-    myGridStyle.disabled = true;
-    myListStyle.disabled = false;
-  } else {
-    myGridStyle.disabled = false;
-    myListStyle.disabled = true;
-  }
+var gridView = document.querySelector("#gridIcon");
+var listView = document.querySelector("#listIcon");
+
+var gridStyle = document.querySelector("#grid");
+var listStyle = document.querySelector("#list");
+
+gridView.addEventListener('click', function () {
+  console.log("*Grid Click*");
+  gridStyle.disabled = false;
+  listStyle.disabled = true;
 });
+
+listView.addEventListener('click', function () {
+  console.log("*List Click*");
+  gridStyle.disabled = true;
+  listStyle.disabled = false;
+});
+
+
+// This will toggle.  I need an action for each icon
+//
+// var mySwitch = document.querySelector("#iconContainer");
+// var myGridStyle = document.querySelector("#grid");
+// var myListStyle = document.querySelector("#list");
+//
+// mySwitch.addEventListener('click', function () {
+//   console.log(myGridStyle.disabled);
+//   if (!myGridStyle.disabled) {
+//     myGridStyle.disabled = true;
+//     myListStyle.disabled = false;
+//   } else {
+//     myGridStyle.disabled = false;
+//     myListStyle.disabled = true;
+//   }
+// });
